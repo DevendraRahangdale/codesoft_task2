8a11fbd13638616bdb6f60329b896241c279ca3b
deven-dra <142200946+deven-dra@users.noreply.github.com>
2023-11-10 16:55:11 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    cout<<"==========**********============"<<endl;
+cout<<"==========**********============";
+cout<<endl;
+cout<<"     CALCULATOR"<<endl;
+
+cout<<"==========**********============"<<endl;
+cout<<"==========**********============";
+cout<<endl;
+}
+
+int main()
+
+{ 
+    int n;
+print(n);
+
+    float a,b;
+    cout<<"enter value of n1 and n2:";
+    cin>>a>>b;
+    char op;
+
+    cout<<"[+   ,  -   ,   *  ,  /   ]"<<endl;
+
+
+    cout<<"enter operator:";
+    cin>>op;
+
+    switch(op)
+    {
+    case '+':
+    cout<<a+b<<endl;
+    break;
+    case '-':
+    cout<<a-b<<endl;
+    break;
+    case '*':
+    cout<<a*b<<endl;
+    break;
+    case '/':
+    cout<<a/b<<endl;
+    break;
+    default:
+    cout<<"enter proprer operator"<<endl;
+  
+    
+   } 
+   cout<<"enter 1 for continue"<<endl;
+   cout<<"enter 0 for exit"<<endl;
+
+   int choice;
+   cin>>choice;
+   if(choice==1){
+        main();
+        }
+        else {
+            exit;
+        }
+   return 0;
+
+}
